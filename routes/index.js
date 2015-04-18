8804e9daa7a44fc40b5bd864a25274db478ee378
Maximilian Ge <mail@maximiliange.com>
2015-04-18 18:14:45 +0100

    server game load page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Great Uni Hack' });
+//});
+
+/* GET GUH ship page. */
+router.get('/', function(req, res) {
+  res.render('ship', { title: 'Great Uni Hack World Ship' })
 });
 
 module.exports = router;
